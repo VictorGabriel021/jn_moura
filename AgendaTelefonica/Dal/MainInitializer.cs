86f5984e80ad773c314c30c085c337effc6ee27d
VictorGabriel021 <victorgabriel.cipa@gmail.com>
2021-02-21 09:16:32 -0300

    Improving the layout

--- AgendaTelefonica/Dal/MainInitializer.cs
+++ AgendaTelefonica/Dal/MainInitializer.cs
@@ -11,20 +11,34 @@ namespace AgendaTelefonica.DAL
     {
         protected override void Seed(MainContext context)
         {
-            var students = new List<People>
+            var contacts = new List<People>
             {
-            new People{Name="Carson",Phone="4396856458",Address="Rua A"},
-            new People{Name="Meredith",Phone="4396856458",Address="Rua B"},
-            new People{Name="Arturo",Phone="4396856458",Address="Rua C"}
+            new People{Name="Victor Gabriel",Phone="(43) 99856-1548",Address="Rua Presidente Castelo Branco 123"},
+            new People{Name="João Paulo",Phone="(43) 98846-7548",Address="Rua Marechal Fonsecca 1200"},
+            new People{Name="Antônio Lopes",Phone="(43) 91856-2548",Address="Rua Coronel Capucho 122"},
+            new People{Name="Gabriel Felipe",Phone="(43) 99856-2548",Address="Rua Benetido 45"},
+            new People{Name="João Paulo",Phone="(43) 98846-7548",Address="Rua Arthur Fonseca 78"},
+            new People{Name="Fernanda Lima",Phone="(43) 98156-1148",Address="Rua Benetido 52"},
+            new People{Name="Jéssica Santos",Phone="(11) 91246-4548",Address="Rua Presidente Castelo Branco 128"},
+            new People{Name="Larissa Silva",Phone="(11) 99511-2348",Address="Rua Arthur Fonseca 85"},
+            new People{Name="Thaís Mendes",Phone="(11) 99242-2348",Address="Rua Coronel Capucho 125"},
+            new People{Name="Renato Rodriguez",Phone="(11) 99811-5648",Address="Rua Coronel Capucho 132"}
             };
 
-            students.ForEach(s => context.Peoples.Add(s));
+            contacts.ForEach(s => context.Peoples.Add(s));
             context.SaveChanges();
             var enrollments = new List<Enrollment>
             {
             new Enrollment{PeopleID=1},
             new Enrollment{PeopleID=2},
-            new Enrollment{PeopleID=3}
+            new Enrollment{PeopleID=3},
+            new Enrollment{PeopleID=4},
+            new Enrollment{PeopleID=5},
+            new Enrollment{PeopleID=6},
+            new Enrollment{PeopleID=7},
+            new Enrollment{PeopleID=8},
+            new Enrollment{PeopleID=9},
+            new Enrollment{PeopleID=10}
             };
             enrollments.ForEach(s => context.Enrollments.Add(s));
             context.SaveChanges();
--- AgendaTelefonica/Models/People.cs
+++ AgendaTelefonica/Models/People.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,16 @@ namespace AgendaTelefonica.Models
     public class People
     {
         public int ID { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [StringLength(100, MinimumLength = 4, ErrorMessage = "Dígito mínimo 4, máximo 100")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [RegularExpression(@"^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$", ErrorMessage = "Número inválido")]
         public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório"), MaxLength(150)]
         public string Address { get; set; }
     }
 }
